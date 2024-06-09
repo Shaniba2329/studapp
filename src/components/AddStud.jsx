d6f618d65d53fdcd04a5019d6ce6da9f7df16e5d
shanibamariyam <shanibamariyam00@gmail.com>
2024-06-09 10:11:24 +0530

    completed

--- src/components/AddStud.jsx
+++ src/components/AddStud.jsx
@@ -3,33 +3,36 @@ import React from 'react'
 const AddStud = () => {
     return (
         <div>
+            <nav />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="control-form">name <input type="text" className="form-control" /></label>
+                                <label htmlFor="" className="form-label">name </label><input type="text" className="form-control" />
+                               </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">admission_no </label><input type="text" className="form-control" />
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-control">admission no <input type="text" className="form-control" /></label>
+                             <label htmlFor="" className="form-label">roll_no</label> <input type="text" className="form-control" />
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-control">rollno <input type="text" className="form-control" /></label>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-control">parent name <input type="text" className="form-control" /></label>
-                            </div>
+                                <label htmlFor="" className="form-label">parent name</label> <input type="text" className="form-control" />
+                               </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-control">college <input type="text" className="form-control" /></label>
-                            </div> <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-control">date of birth <input type="date" name="" id="" className="form-control" /></label>
-                            </div> <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-control">email_id <input type="text" className="form-control" /></label>
-                            </div> <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-control">password <input type="text" className="form-control" /></label>
-                            </div> <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-control">confirm password<input type="text" className="form-control" /></label>
-                            </div>
+                                <label htmlFor="" className="form-label">college</label>
+                                     <input type="text" className="form-control" />
+                                </div> 
+                                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                    <label htmlFor="" className="form-label">date of birth </label><input type="date" name="" id="" className="form-control" />
+                                </div> <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                    <label htmlFor="" className="form-label">email_id</label> <input type="text" className="form-control" />
+                                </div> <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                    <label htmlFor="" className="form-label">password</label> <input type="text" className="form-control" />
+                                </div> <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                    <label htmlFor="" className="form-label">confirm password</label> <input type="text" className="form-control" />
+                               </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                                 <buttun className="btn btn-success">register</buttun>
                             </div>
@@ -40,7 +43,7 @@ const AddStud = () => {
                     </div>
                 </div>
             </div>
-            AddStud</div>
+        </div>
     )
 }
 
